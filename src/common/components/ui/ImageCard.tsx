e52fc901e39b684ece1bc683a524dc81442e8c79
imdeepss <deepakk@geeconglobal.com>
2024-04-27 20:25:14 +0530

    Fix: grid Ui

--- src/common/components/ui/ImageCard.tsx
+++ src/common/components/ui/ImageCard.tsx
@@ -12,23 +12,26 @@ const ImageCard = ({ src, alt }: ImageType) => {
   const imageURLRef = useRef<HTMLImageElement>(null);
 
   return (
-    <div className="">
-      <div className="relative">
-        <Image
-          width={500}
-          height={500}
-          className="h-auto max-w-full rounded-lg bg-grey"
-          src={src}
-          alt={alt}
-          ref={imageURLRef}
-        />
-        {src &&
-          <div className="">
-            <OpenButton src={src} alt={alt} />
-            <DownloadButton src={imageURLRef} alt={alt} />
-          </div>
-        }
-      </div>
+    <div className="after:content after:shadow-highlight group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg">
+      <Image
+        src={src}
+        alt={alt}
+        ref={imageURLRef}
+        width={720}
+        height={480}
+        sizes="(max-width: 640px) 100vw,
+              (max-width: 1280px) 50vw,
+              (max-width: 1536px) 33vw,
+              25vw"
+        className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+        style={{ transform: "translate3d(0, 0, 0)" }}
+      />
+      {src && (
+        <div className="">
+          <OpenButton src={src} alt={alt} />
+          <DownloadButton src={imageURLRef} alt={alt} />
+        </div>
+      )}
     </div>
   );
 };
--- src/common/components/ui/ImageGallery.tsx
+++ src/common/components/ui/ImageGallery.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import { getImagesUnsplashAPI } from '@/common/api';
-import ImageCard from './ImageCard';
-import { ImageType, SearchInputType } from '@/common/type';
+import { useEffect, useState } from "react";
+import { getImagesUnsplashAPI } from "@/common/api";
+import ImageCard from "./ImageCard";
+import { ImageType, SearchInputType } from "@/common/type";
 
 const ImageGallery = () => {
   const [imageDetails, setImageDetails] = useState<ImageType[] | null>(null);
@@ -11,11 +11,11 @@ const ImageGallery = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const searchInput: SearchInputType = { search: '' };
+        const searchInput: SearchInputType = { search: "" };
         const details = await getImagesUnsplashAPI(searchInput);
         setImageDetails(details);
       } catch (error) {
-        console.error('An error occurred while fetching image details:', error);
+        console.error("An error occurred while fetching image details:", error);
         setImageDetails(null);
       }
     };
@@ -27,27 +27,15 @@ const ImageGallery = () => {
     return <></>;
   }
 
-  const columns = 4;
-  const columnData = Array.from({ length: columns }, (_, i) =>
-    imageDetails.slice(
-      i * (imageDetails.length / columns),
-      (i + 1) * (imageDetails.length / columns)
-    )
-  );
-
   return (
     <section className="px-4 py-10 md:px-20">
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-        {columnData.map((column, columnIndex) => (
-          <div className="flex flex-col gap-4" key={columnIndex}>
-            {column.map((image, imageIndex) => (
-              <ImageCard
-                key={imageIndex}
-                src={image?.urls?.regular}
-                alt={image?.alt_description ?? 'images'}
-              />
-            ))}
-          </div>
+      <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
+        {imageDetails.map(({ id, urls, alt_description }) => (
+          <ImageCard
+            key={id}
+            src={urls?.regular}
+            alt={alt_description ?? "images"}
+          />
         ))}
       </div>
     </section>
